f7ac1ce758a6753ec47faeb2a803ccaca09b50b9
Mikhail Gorshenin <m.gorshenin@profsoft.pro>
2018-07-27 17:10:20 +0400

    fix bottomnavigationview

--- app/src/main/java/online/profsoft/love2gether/MainActivity.java
+++ app/src/main/java/online/profsoft/love2gether/MainActivity.java
@@ -1,13 +1,44 @@
 package online.profsoft.love2gether;
 
+import android.app.ActionBar;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import online.profsoft.love2gether.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+           switch (item.getItemId()) {
+               case R.id.action_swipecard:
+                   return true;
+               case R.id.action_chat:
+                   return true;
+               case R.id.action_profile:
+                   return true;
+           }
+           return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        if(getActionBar() != null){
+            getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            getSupportActionBar().setCustomView(R.layout.abs_layout);
+        }
     }
 }
